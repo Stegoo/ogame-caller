ef49af7f69035083a7e9bb2c551aeb95b7d71b5e
Stego <alexandre.nucera@gmail.com>
2016-06-09 17:07:23 +0200

    add end of line in the generated call file

--- file_handler.py
+++ file_handler.py
@@ -8,11 +8,11 @@ def write_message(message):
 
 def write_call_file(mobile_number=config.mobile_number, max_retries=5, retry_time=300, wait_time=45, context="ogame_attack_warning"):
     f = open(config.call_file, 'w+')
-    f.write("Channel: SIP/%s@provider" % mobile_number)
-    f.write("MaxRetries: %s" % max_retries)
-    f.write("RetryTime: %s" % retry_time)
-    f.write("WaitTime: %s" % wait_time)
-    f.write("Context: %s" % context)
-    f.write("Extension: %s" % mobile_number)
-    f.write("Priority: 1")
+    f.write("Channel: SIP/%s@provider\n" % mobile_number)
+    f.write("MaxRetries: %s\n" % max_retries)
+    f.write("RetryTime: %s\n" % retry_time)
+    f.write("WaitTime: %s\n" % wait_time)
+    f.write("Context: %s\n" % context)
+    f.write("Extension: %s\n" % mobile_number)
+    f.write("Priority: 1\n")
     f.close()
