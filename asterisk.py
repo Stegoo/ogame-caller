9e77443f5eb9d1e2723a9cd8dc6609aa1a8309e4
Stego <alexandre.nucera@gmail.com>
2016-06-09 17:05:21 +0200

    write call file

--- asterisk.py
+++ asterisk.py
@@ -7,4 +7,5 @@ def call(msg=config.default_message):
     file_handler.write_message(msg)
     #copy the call file into asterisk folder for outgoing call
     #copy permissions but not meta data, consider using shutil.copy2 if you need them
+    file_handler.write_call_file()
     shutil.copy(config.call_file, config.asterisk_outgoing)
